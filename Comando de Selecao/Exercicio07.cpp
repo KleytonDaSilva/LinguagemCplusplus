ca8a03cf5cafdfdd8da43ff5574cc86159b3a082
nksaddam <nksaddam@gmail.com>
2024-01-22 21:05:27 -0300

    atualizacao 22/01/2024 21h:05min

--- Comando de Selecao/Exercicio07.cpp
+++ Comando de Selecao/Exercicio07.cpp
@@ -9,7 +9,7 @@ int main() {
     int x,y,z;
     cout<<"Informe os valor de X,Y e Z"<<"\n";
     cin>>x>>y>>z;
-    if(x != y && y != z){
+    if(x != y && y != z && x != z){
         cout<<"Triangulo Escaleno!";
     }else{
         if(x == y && x == z && y == z){
